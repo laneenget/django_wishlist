a2e39f881f1e785799ea286ad217447cd4cbb9a8
Lane Enget <lane.enget@go.minneapolis.edu>
2020-04-06 22:12:52 -0500

    Image addition

--- wishlist/travel_wishlist/forms.py
+++ wishlist/travel_wishlist/forms.py
@@ -5,3 +5,15 @@ class NewPlaceForm(forms.ModelForm):
     class Meta:
         model = Place
         fields = ('name', 'visited')
+
+# Create a custome date input field
+class DateInput(forms.DateInput):
+    input_type = 'date' # Override the text input type
+
+class TripReviewForm(forms.ModelForm):
+    class Meta:
+        model = Place
+        fields = ('notes', 'date_visited', 'photo')
+        widgets = {
+            'date_visited': DateInput()
+        }
--- wishlist/travel_wishlist/views.py
+++ wishlist/travel_wishlist/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Place
-from .forms import NewPlaceForm
+from .forms import NewPlaceForm, TripReviewForm
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseForbidden
+from django.contrib import messages
 
 # Create your views here.
 
 # Handle requests to home page
+@login_required
 def place_list(request):
     """If this is a POST request, the user clicked the Add button
     in the form. Check if the new place is valid, if so, save a
@@ -17,25 +22,67 @@ def place_list(request):
 
     if request.method == 'POST':
         form = NewPlaceForm(request.POST)
-        place = form.save() # Create a new Place from the form
+        place = form.save(commit=False) # Create a new Place from the form
+        place.user = request.user # Associate the place with logged-in user
         if form.is_valid(): # Checks against DB constraints
             place.save()    # Saves to the database
             return redirect('place_list') # Redirects to GET view with name place_list
 
     # if not a POST, or the form is not valid, render the page
     # with the form to add a new place, and list of places
-    places = Place.objects.filter(visited=False).order_by('name') #fetch data w/ appropriate filter
+    places = Place.objects.filter(user=request.user).filter(visited=False).order_by('name') #fetch data w/ appropriate filter
     new_place_form = NewPlaceForm()
     return render(request, 'travel_wishlist/wishlist.html', { 'places': places, 'new_place_form': new_place_form })
 
+@login_required
 def places_visited(request):
-    visited = Place.objects.filter(visited=True)
+    visited = Place.objects.filter(user=request.user).filter(visited=True)
     return render(request, 'travel_wishlist/visited.html', { 'visited': visited })
 
+@login_required
 def place_was_visited(request, place_pk):
     if request.method == 'POST':
         place = get_object_or_404(Place, pk=place_pk)
-        place.visited = True
-        place.save()
+        print(place.user, request.user)
+        if place.user == request.user:
+            place.visited = True
+            place.save()
+        else:
+            return HttpResponseForbidden()
 
     return redirect('place_list')
+
+@login_required
+def place_details(request, place_pk):
+    place = get_object_or_404(Place, pk=place_pk)
+
+    if place.user != request.user:
+        return HttpResponseForbidden()
+
+    if request.method == 'POST':
+        form = TripReviewForm(request.POST, request.FILES, instance=place)
+
+        if form.is_valid():
+            form.save()
+            messages.info(request, 'Trip information updated!')
+        else:
+            messages.error(request, form.errors)
+
+        return redirect('place_details', place_pk=place_pk)
+
+    else:
+        if place.visited:
+            review_form = TripReviewForm(instance=place)
+            return render(request, 'travel_wishlist/place_detail.html', {'place': place, 'review_form': review_form} )
+
+        else:
+            return render(request, 'travel_wishlist/place_detail.html', {'place': place})
+
+@login_required
+def delete_place(request, place_pk):
+    place = get_object_or_404(Place, pk=place_pk)
+    if place.user == request.user:
+        place.delete()
+        return redirect('place_list')
+    else:
+        return HttpResponseForbidden()
